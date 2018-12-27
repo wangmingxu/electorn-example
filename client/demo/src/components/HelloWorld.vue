5636d50f0b3491e3423aec6e65f6f7ef88d19196
王名旭 <1220712534@qq.com>
2018-12-27 17:14:00 +0800

    commit

---
+++ client/demo/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <button @click="test">点击跳转回electorn主页</button>
+  </div>
+</template>
+
+<script>
+import { ipcRenderer } from 'electron'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    test () {
+      ipcRenderer.send('resume')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
